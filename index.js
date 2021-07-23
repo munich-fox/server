26c0f9eca52fb1fcf071a83f0fc9c7393f01e280
Ajat Darojat <ajatdarojat45@gmail.com>
2021-07-23 14:23:53 +0700

    add login feature

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.post("/login", (req, res) => {
+	const { email, password } = req.body;
+
+	res.status(200).json({token: "rahasia"})
+});
+
 app.listen(PORT, () => {
 	console.log(`server liston on PORT: ${PORT}`);
 });
